3f314c8ad7751ea63c9120be59f378828f551cf7
Mahmud0808 <crazymahmud08@gmail.com>
2020-09-22 15:18:55 +0600

    Create fw.py

---
+++ userge/plugins/misc/fw.py
@@ -0,0 +1,13 @@
+from userge import userge, Message
+
+LOG = userge.getLogger(__name__)
+
+CHANNEL = userge.getCLogger(__name__)
+
+@userge.on_cmd("fw", about="Global Stable 11.0.1.0 Firmware")
+async def testing(message: Message):
+   LOG.info("Starting fw command...")
+
+   await message.edit("Firmware:\nhttps://drive.google.com/file/d/1VFtHwRp6nrHsKFq_tqk9rdmtO-J7NDMB/view?usp=sharing")
+
+   await CHANNEL.log("fw command successfully executed!")
